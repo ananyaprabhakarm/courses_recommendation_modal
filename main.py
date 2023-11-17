fca2bcf6a999a4420d13b05fb70ee31662659458
ananyaprabhakarm <ananyaprabhakarm@gmail.com>
2023-11-18 00:22:18 +0530

    some updation done

--- main.py
+++ main.py
@@ -10,10 +10,17 @@ df = pd.read_csv('DataSet.csv')
 
 app = FastAPI()
 
-# app.add_middleware(
-#    CORSMiddleware,
-   
-# )
+origins = [
+   "http://localhost:5173",
+]
+
+app.add_middleware (
+   CORSMiddleware,
+   allow_origins = origins,
+   allow_credentials = True,
+   allow_methods = ["*"],
+   allow_headers = ["*"],
+)
 
 new_df = df.copy()
 
